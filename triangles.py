87aaaf389f0bc06006a24f2103ce45779486a90b
Amanda <azheng1@ucvts.org>
2018-12-13 01:27:45 +0000

    added triangles

---
+++ triangles.py
@@ -0,0 +1,5 @@
+import math  
+
+s1 = int(input("enter the first side length (not hypotenuse)"))
+s2 = int(input("enter the second side length (not hypotenuse)"))
+print(f"the hypotenuse is: {math.sqrt(s1*s1+s2*s2)}")
